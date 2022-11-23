ecad49fc2e7a08ef034f7541473f2dbdb1eaa388
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2022-11-23 23:41:35 +0530

    add the VideoCapture Function
    - Using Opencv

--- main.py
+++ main.py
@@ -4,8 +4,35 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
+cam = cv2.VideoCapture(0)   # 0 -> index of camera
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("test", frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+
 # read the image
-image = cv2.imread('upfront.jpg')
+image = cv2.imread('opencv_frame_0.png')
 
 # resize image
 image = imutils.resize(image, width=500)
